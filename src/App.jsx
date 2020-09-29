64473897d5056530f66e24f2c018ad872b177df7
Sanjay <54913971+San1ay@users.noreply.github.com>
2020-09-30 01:08:40 +0545

    Removed some redundant code

--- src/App.jsx
+++ src/App.jsx
@@ -36,13 +36,7 @@ export default function App() {
         });
     };
     getCountriesData();
-  }, []);
-  useEffect(() => {
-    fetch("https://disease.sh/v3/covid-19/all")
-      .then((res) => res.json())
-      .then((data) => {
-        setCountryInfo(data);
-      });
+    onCountryChange("worldwide");
   }, []);
 
   const onCountryChange = async (e) => {
